28dac5d755cae1f4894cfd2d38012037799a4b6c
Evren671 <evren67c@gmail.com>
2025-12-11 19:46:30 +0100

    ein tutorial hinzugefügt

--- src/Tutorials.tsx
+++ src/Tutorials.tsx
@@ -8,6 +8,10 @@ const Tutorials = () => {
     const [selectedTutorial, setSelectedTutorial] = useState('add-devices');
 
     const tutorials = {
+        'get-premium': {
+            title: 'How to get Premium Access',
+            content: '1. Click on your profile in the top right corner\n2. Look for a button called "Get Premium"\n3. Click on the button and accept the conditions and click on buy subscription'
+        },
         'add-devices': {
             title: 'How to Add Devices',
             content: 'EMPTY'
@@ -15,6 +19,11 @@ const Tutorials = () => {
     };
 
     const menuItems = [
+        {
+            key: 'get-premium',
+            icon: <BookOutlined />,
+            label: 'How to get Premium Access',
+        },
         {
             key: 'add-devices',
             icon: <BookOutlined />,
@@ -44,7 +53,7 @@ const Tutorials = () => {
                         <Title level={3}>
                             {tutorials[selectedTutorial as keyof typeof tutorials].title}
                         </Title>
-                        <Paragraph style={{ fontSize: 16, lineHeight: 1.8 }}>
+                        <Paragraph style={{ whiteSpace: 'pre-line', fontSize: 16, lineHeight: 1.8 }}>
                             {tutorials[selectedTutorial as keyof typeof tutorials].content}
                         </Paragraph>
                     </Card>
